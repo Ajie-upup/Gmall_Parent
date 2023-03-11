a65592d621f8483a22b38303eb96118ead734bc5
ajie <1651588782@qq.com>
2023-03-11 16:42:29 +0800

    配置网关跨域,上传服务至nacos,在nacos中配置各服务配置文件

---
+++ server-gateway/src/main/java/com/ajie/gmall/gateway/ServerGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.ajie.gmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ajie
+ * @date 2023/3/11
+ * @description:
+ */
+@SpringBootApplication
+public class ServerGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGatewayApplication.class, args);
+    }
+}
